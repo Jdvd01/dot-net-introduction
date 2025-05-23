d8798324e6033d5e77c07b743933bbee0d4ed4c4
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-05-23 18:57:40 -0400

    feat: Interfaces

---
+++ interfaces/Program.cs
@@ -0,0 +1,6 @@
+﻿CalculatorService calculator = new CalculatorService();
+
+Console.WriteLine("Addition: " + calculator.Add(5, 3));
+Console.WriteLine("Subtraction: " + calculator.Subtract(5, 3));
+Console.WriteLine("Multiplication: " + calculator.Multiply(5, 3));
+Console.WriteLine("Division: " + calculator.Divide(5, 3));
