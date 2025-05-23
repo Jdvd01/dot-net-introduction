49e932435d77115c3eaae02be675134792825090
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-05-23 19:08:42 -0400

    feat: logger example for interfaces

---
+++ logger/Program.cs
@@ -0,0 +1,5 @@
+﻿ILogger consoleLogger = new ConsoleLogger();
+consoleLogger.Log("This is a log message to the console");
+
+ILogger fileLogger = new FileLogger();
+fileLogger.Log("This is a log message to a file");
